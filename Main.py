a043f790a0a9f031823bf85a697ff5923f2f1b6c
ThatGoblinKing <gusjminecritter@gmail.com>
2023-10-26 10:30:36 -0600

    Added rudementary animation

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import pygame
 import Player
 import Platform
-import math
 pygame.init()
 pygame.display.set_caption("easy platformer")  # sets the window title
 screen = pygame.display.set_mode((800, 800))  # creates game screen
@@ -9,23 +8,10 @@ screen.fill((0, 0, 0))
 clock = pygame.time.Clock()  # set up clock
 gameover = False  # variable to run our game loop
 
-
-# player variables
-xpos = 500  # xpos of player
-ypos = 200  # ypos of player
-vx = 0  # x velocity of player
-vy = 0  # y velocity of player
-# this list holds whether each key has been presse
-# this variable stops gravity from pulling you down more when on a platform
-isOnGround = False
-isOnBouncePad = False
-
 player1 = Player.Player(pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, (0,0,255))
 player2 = Player.Player(pygame.K_a, pygame.K_d, pygame.K_w, (255,0,0))
 
-
-platforms = [Platform.Platform((400, 700), (500, 760), (255, 69, 169)), Platform.Platform(
-    (0, 800), (800, 825), (0, 0, 0))]
+platforms = [Platform.Platform((0, 700), (100, 760), (255, 255, 255))]
 
 
 while not gameover:  # GAME LOOP############################################################
@@ -43,10 +29,10 @@ while not gameover:  # GAME LOOP################################################
 
     screen.fill((0, 0, 0))  # wipe screen so it doesn't smear
 
-    for platform in platforms:
-        platform.draw(screen)
     player1.draw(screen)
     player2.draw(screen)
+    for platform in platforms:
+        platform.draw(screen)
 
     pygame.display.flip()  # this actually puts the pixel on the screen
 
--- Player.py
+++ Player.py
@@ -8,16 +8,24 @@ UP = 2
 DOWN = 3
 JOM = pygame.mixer.Sound("VineBoom.mp3")
 SIZE_X = 40
-SIZE_Y = 80
+SIZE_Y = 92
 GRAVITY_ITERATION = .15
 HOVER_TOLERANCE = 5
 JUMP_CONTROL = 0.2
-COLLISION_TOLERANCE = 10
-
+COLLISION_TOLERANCE = 15
+SPRITE_SHEET = pygame.transform.scale(pygame.image.load('RunningSpriteSheetFull.png'), (161 * 4, 46 * 4))
+FRAME_WIDTH = 92
+FRAME_HEIGHT = 92
+RUN_CYCLE_LENGTH = 875
+FRAME_HOLD_LENGTH = 125
+SPRITE_OFFSET = 25
 
 class Player:
     def __init__(self, left, right, up, color):
         print("New player made!")
+        self.rowNum = 0
+        self.frameNum = 0
+        self.ticker = 0
         self.xpos = 500  # xpos of player
         self.ypos = 200  # ypos of player
         self.vx = 0  # x velocity of player
@@ -36,11 +44,23 @@ class Player:
         self.moveSpeed = 6
         self.jumpDirection = NONE
         self.rect = pygame.Rect((self.xpos, self.ypos), (SIZE_X,SIZE_Y))
-        self.jumpHeight = 22
+        self.jumpHeight = 16
         self.ground = 800
+        self.platformLeft = 0
+        self.platformRight = 0
+        
+    def animate(self):
+        if self.vx < 0:
+            frameNum = int((pygame.time.get_ticks() % RUN_CYCLE_LENGTH) / FRAME_HOLD_LENGTH)
+            self.rowNum = 1
+        elif self.vx > 0:
+            frameNum = int((pygame.time.get_ticks() % RUN_CYCLE_LENGTH) / FRAME_HOLD_LENGTH)
+            self.rowNum = 0
+        else: frameNum = 0
+        self.spriteProperties = (FRAME_WIDTH * frameNum, self.rowNum * FRAME_HEIGHT, FRAME_WIDTH, FRAME_HEIGHT)
+            
 
     def move(self, gameEvents, platforms):
-        startingVx = self.vx
 
         for event in gameEvents:  # quit game if x is pressed in top corner
             if event.type == pygame.KEYDOWN:  # keyboard input
@@ -102,23 +122,26 @@ class Player:
             elif self.jumpDirection == RIGHT:
                 self.vx += 0.1
                 
+        self.predictedRect = pygame.Rect(pygame.Rect((self.xpos + self.vx, self.ypos + self.vy), (SIZE_X,SIZE_Y)))
+            
         for platform in platforms:
             self.collide(platform.getRect())
-            self.predictedRect = pygame.Rect(pygame.Rect((self.xpos + self.vx, self.ypos + self.vy), (SIZE_X,SIZE_Y)))
+
         
         if self.colliding[UP] and self.vy < 0 or self.colliding[DOWN] and self.vy > 0:
-            self.ypos -= self.vy
             self.vy = 0
+        else: self.ypos += self.vy
         if self.colliding[LEFT] and self.vx < 0 or self.colliding[RIGHT] and self.vx > 0:
-            self.xpos -= self.vx
             self.vx = 0
+        else: self.xpos += self.vx
                 
         # update player position
-        self.xpos += self.vx
-        self.ypos += self.vy
+        self.animate()
         self.rect = pygame.Rect(pygame.Rect((self.xpos, self.ypos), (SIZE_X,SIZE_Y)))
         
     def touchGround(self):
+        if self.xpos + SIZE_X < self.platformLeft or self.xpos > self.platformRight: 
+            self.ground = 800
         if self.ypos + SIZE_Y + self.vy > self.ground or self.isOnGround:
             self.isOnGround = True
             self.vy = 0
@@ -127,16 +150,22 @@ class Player:
 
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
+        #pygame.draw.rect(screen, (255,255,255), self.predictedRect)
+        screen.blit(SPRITE_SHEET, (self.xpos - SPRITE_OFFSET, self.ypos), self.spriteProperties)
+        #pygame.draw.rect(screen, (0,255,0), self.rect, 2) <--- HITBOX
     
     def collide(self, otherRect):
-        if not pygame.Rect.colliderect(self.rect, otherRect):
+        if not pygame.Rect.colliderect(self.predictedRect, otherRect):
             self.colliding = [False, False, False, False]
             return
-        self.colliding[LEFT] = abs(self.predictedRect.left - otherRect.right) < COLLISION_TOLERANCE
-        self.colliding[RIGHT] = abs(self.predictedRect.right - otherRect.left) < COLLISION_TOLERANCE
-        self.colliding[UP] = abs(self.predictedRect.top - otherRect.bottom) < COLLISION_TOLERANCE
-        self.colliding[DOWN] = abs(self.predictedRect.bottom - otherRect.top) < COLLISION_TOLERANCE
+        
+        self.platformLeft = otherRect.left
+        self.platformRight = otherRect.right
+
+        self.colliding[LEFT] = abs(self.predictedRect.left + self.vx - otherRect.right) < COLLISION_TOLERANCE
+        self.colliding[RIGHT] = abs(self.predictedRect.right + self.vx - otherRect.left) < COLLISION_TOLERANCE
+        self.colliding[UP] = abs(self.predictedRect.top + self.vy - otherRect.bottom) < COLLISION_TOLERANCE
+        self.colliding[DOWN] = abs(self.predictedRect.bottom + self.vy - otherRect.top) < COLLISION_TOLERANCE
         if self.colliding[DOWN] or self.colliding == [False, False, False, False]:
             self.ground = otherRect.top
         else:
@@ -147,4 +176,5 @@ class Player:
             self.xpos = otherRect.left - SIZE_X
         if self.colliding[UP]:
             self.ypos = otherRect.bottom 
+            print ("hit bottom")
         print(self.colliding)
